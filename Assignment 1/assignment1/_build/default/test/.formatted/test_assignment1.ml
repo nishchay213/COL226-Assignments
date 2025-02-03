caefa4ef1330c9accc90b60e5803174fba8efdd1
Nishchay Patil <patilnishchay23.com>
2025-02-03 11:43:59 +0530

    formatting

---
+++ Assignment 1/assignment1/_build/default/test/.formatted/test_assignment1.ml
@@ -0,0 +1,8 @@
+open Assignment1.Vector
+
+ (* let _str = Sexplib.Sexp.to_string_hum (sexp_of_vector (scale 2.5 (create 10000000 2.2))) *)
+
+
+let _vec1 = create 3 2.0
+let () = Printf.printf("Finished\n") 
+(* let _ = testcase1_addv() *)
---
+++ Toy Calculator/toycalculator/_build/default/lib/.formatted/toy.mli
@@ -0,0 +1,13 @@
+type myBool = T | F
+type exp = Num of int | B1 of myBool | Plus of exp*exp | Times of exp*exp | And of exp*exp | Or of exp*exp | Not of exp
+| Eq of exp*exp | Gt of exp*exp
+type values = N of int | B of bool;;
+type opcode = LDN of int | LDB of myBool | PLUS | TIMES | AND | OR | NOT | EQ | GT
+exception Wrong;;
+val myBool2bool : myBool -> bool
+val size : exp -> int 
+val ht : exp -> int
+val posttrav : exp -> string
+val eval : exp -> values
+val compile : exp -> opcode list
+val stackmc : exp list -> opcode list -> exp
